69f5aa1d9331b9ef9e8f2d6e50ae84186f88bca4
Ravi Longia <ravilongia@hotmail.com>
2025-01-21 22:00:38 +1100

    new files

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,22 @@
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be atleast 18" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
@@ -15,17 +31,21 @@ const Form = () => {
           type="text"
           className="form-control"
         />
+        {errors.name && (
+          <p className="mb-3 text-danger">{errors.name.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="mb-3 text-danger">{errors.age.message}</p>}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
